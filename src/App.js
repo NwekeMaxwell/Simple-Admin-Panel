dff581478af4a2023d1165f4eee5d5f6aa6bbdf9
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-05-02 06:07:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import MainDash from "./Components/MainDash/MainDash";
+import RightSide from "./Components/RightSide/RightSide";
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar />
+        <MainDash />
+        <RightSide />
+      </div>
+    </div>
+  );
+}
+
+export default App;
